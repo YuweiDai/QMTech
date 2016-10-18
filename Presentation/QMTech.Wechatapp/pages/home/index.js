761fa3d5d17bad32009c193929c8ff88d9eed9d1
戴煜炜 <kingdyw@126.com>
2016-10-18 23:56:58 +0800

    修改页面布局

--- Presentation/QMTech.Wechatapp/pages/home/index.js
+++ Presentation/QMTech.Wechatapp/pages/home/index.js
@@ -1,15 +1,23 @@
-Page({
+﻿Page({
     data: {
         background: ['http://img.waimai.baidu.com/pb/0e0d97764c447207ed3c08834089b7c96e@w_750,q_90',
          'http://img.waimai.baidu.com/pb/4225d918b283ebb1060548bb1875e040d0',
           '../../image/banner3.jpg'],
         indicatorDots: true,
         vertical: false,
-        autoplay: false,
+        autoplay: true,
         interval: 3000,
         duration: 1200,
-
-        top_line_indicatorDots:false
+         
+        top_lines: ['儿童身高, 体重发育标准! 快来看看0...', '儿童身高, 体重发育标准! 快来看看1...', ],
+        shops: [
+            {
+                name: "测试商家一",
+                star: 5,
+                sales: 55,
+                pic: ""
+            }
+        ]
     },
     changeIndicatorDots: function (e) {
         this.setData({
