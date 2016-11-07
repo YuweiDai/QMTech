4a5e97ac82c13d1d3e23882aecb9e123cef4704d
戴煜炜 <77827981@qq.com>
2016-11-07 17:09:19 +0800

    微信端修改

--- Presentation/QMTech.Wechatapp/app.js
+++ Presentation/QMTech.Wechatapp/app.js
@@ -2,7 +2,23 @@ var common = require('utils/util.js');
 
 //app.js
 App({
-  onLaunch: function () {
+    onLaunch: function () {
+
+        wx.login({
+            success: function(res) {
+                if (res.code) {
+                    //发起网络请求
+                    wx.request({
+                        url: 'https://test.com/onLogin',
+                        data: {
+                            code: res.code
+                        }
+                    })
+                } else {
+                    console.log('获取用户登录态失败！' + res.errMsg)
+                }
+            };
+
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
